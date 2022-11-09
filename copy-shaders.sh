9ae8f0c55ad5089a4caa83478c3aab15c8ed0eeb
Rakarake <williambodin2001@gmail.com>
2022-11-09 14:45:06 +0100

    Copy shaders yes

--- copy-shaders.sh
+++ copy-shaders.sh
@@ -1,2 +1,17 @@
 cp lab1-rasterization/simple.frag build/lab1-rasterization
 cp lab1-rasterization/simple.vert build/lab1-rasterization
+
+cp lab2-textures/simple.* build/lab2-textures
+
+cp lab3-camera/simple.vert build/lab3-camera
+cp lab3-camera/simple.frag build/lab3-camera
+
+cp lab4-shading/*.frag build/lab4-shading
+cp lab4-shading/*.vert build/lab4-shading
+
+cp lab5-rendertotexture/*.vert build/lab5-rendertotexture
+cp lab5-rendertotexture/*.frag build/lab5-rendertotexture
+
+cp lab6-shadowmaps/*.vert build/lab6-shadowmaps
+cp lab6-shadowmaps/*.frag build/lab6-shadowmaps
+
